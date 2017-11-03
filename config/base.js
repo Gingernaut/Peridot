1fd00933f809a8527a756b418a34fc9e6d773289
Tyler Peterson <tylermpeterson36@gmail.com>
2017-11-02 23:41:02 -0600

    added new packaged and updated dependencies

--- config/base.js
+++ config/base.js
@@ -3,6 +3,17 @@
   Import the 'oracle' file anywhere you need configuration options.
 */
 
+// https://www.npmjs.com/package/envalid
+/*
+const envalid = require('envalid')
+const { str, email, json } = envalid
+
+const env = envalid.cleanEnv(process.env, {
+  API_KEY: str(),
+  ADMIN_EMAIL: email({ default: 'admin@example.com' }),
+  EMAIL_CONFIG_JSON: json({ desc: 'Additional email parameters' })
+})
+*/
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   port: process.env.SERVER_PORT || 8080,
