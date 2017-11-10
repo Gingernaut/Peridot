040829506b5e323cff6d1b23d8bb3670f7f28c56
Tyler Peterson <tylermpeterson36@gmail.com>
2017-11-10 01:12:05 -0700

    eslintrc updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,21 +13,25 @@ module.exports = {
   plugins: [
     'html'
   ],
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+
   extends: [
+    'eslint:recommended',
     'standard'
   ],
-  // add your custom rules here
+
   rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 1,
     // console log statements
-    'no-console': 0,
+    'no-console': 1,
 
-    'indent': [2, 2]
+    'indent': [2, 2],
+    // triple equality
+    'eqeqeq': 1,
+    'no-var': 2
   }
 }
