ebef184afb0911234828749ecbaed3e202413a6d
Tyler Peterson <tylermpeterson36@gmail.com>
2019-10-26 18:15:18 -0700

    bumped sass loader

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: fs.readFileSync("./src/styles/_variables.scss", "utf-8"),
+        prependData: fs.readFileSync("./src/styles/_variables.scss", "utf-8"),
       },
     },
   },
